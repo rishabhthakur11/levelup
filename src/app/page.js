8e0371e5411f12812b772d525631c28f05a98866
Rishabh Thakur <rishabhthakur046@gmail.com>
2023-11-09 23:45:38 +0530

    Added Logo Cloud on landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
 import HeroSection from "@/components/LandingpageComponents/HeroSection/HeroSection";
+import LogoCloud from "@/components/LandingpageComponents/LogoCloud/LogoCloud";
 import Navbar from "@/components/LandingpageComponents/NavBar/Navbar";
 
 export default function Home() {
   return (
-    <main className="absolute bg-backgroundColor h-full w-full">
-      <div className="max-w-7xl mx-auto">
+    <main className="bg-backgroundColor h-fit w-full">
+      <div className="max-w-7xl mx-auto sticky">
         <Navbar />
       </div>
-      <div className="">
+      <div>
         <HeroSection />
+      </div>
+      <div className="mt-12 lg:mt-32">
+        <LogoCloud />
       </div>
     </main>
   );
