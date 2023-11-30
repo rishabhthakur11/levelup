75c6ad1b4fa02427d71aee57d96628f60bbfaf7c
Rishabh Thakur <rishabhthakur046@gmail.com>
2023-11-30 19:10:50 +0530

    added prisma to the project along with course form creation

--- app/(LMS)/teacher/courses/page.jsx
+++ app/(LMS)/teacher/courses/page.jsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 function CoursePage() {
-  return <div>CoursePage</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
 }
 
 export default CoursePage;
