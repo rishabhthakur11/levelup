ca7a5397210925328c24392d9133fed41ca6d18f
Rishabh Thakur <rishabhthakur046@gmail.com>
2023-12-02 00:13:54 +0530

    created form for adding description of course

--- app/(LMS)/teacher/courses/[courseId]/page.jsx
+++ app/(LMS)/teacher/courses/[courseId]/page.jsx
@@ -5,6 +5,7 @@ import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import React from "react";
 import TitleForm from "./_components/TitleForm";
+import DescriptionForm from "./_components/DescriptionForm";
 
 async function courseIdPage({ params }) {
   const { userId } = auth();
@@ -43,6 +44,7 @@ async function courseIdPage({ params }) {
               <h2 className="text-xl">Customize your course</h2>
             </div>
             <TitleForm initialData={course} courseId={course.id} />
+            <DescriptionForm initialData={course} courseId={course.id} />
           </div>
         </div>
       </div>
